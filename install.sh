008bd805b25d0c0e15779aca61d249dee8058292
Liu Qing <winglq@gmail.com>
2016-08-13 23:24:03 +0800

    deamon not right
    Signed-off-by: Liu Qing <winglq@gmail.com>

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
-#!/bin/bash
-
-
-set -e
-
 pip install .
 cat > /bin/start_tomato.sh << EOF
+#!/bin/sh
 tomato
 EOF
 
 chmod +x /bin/start_tomato.sh
+chown qing /bin/start_tomato.sh
 
 cp etc/init.d/tomato /etc/init.d/tomato
 update-rc.d tomato defaults
